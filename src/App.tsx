8dd15e4ac2a77491a31d56fd6fe12d375c6a06ee
19Famil <129958208+19Famil@users.noreply.github.com>
2023-05-01 16:46:45 +0400

    KalkulyatorReact-Native

--- src/App.tsx
+++ src/App.tsx
@@ -1,93 +1,15 @@
-// import React from 'react';
-// import {View, Image, StyleSheet, Text, SafeAreaView} from 'react-native';
-// import HomePage from './HomePage';
-// import SecondPage from './SecondPage';
-// import Calculute from './Calculute';
-// const App = () => {
-//   return (
-//     <SafeAreaView>
-// <HomePage />
-// {/* <SecondPage /> */}
-//     </SafeAreaView>
-//   );
-// };
-// export default App;
 
-// import React  from 'react';
-// import { useEffect, useState } from 'react';
-// import {StyleSheet, Text, SafeAreaView, TouchableOpacity} from 'react-native';
-// // import { useEffect, useState } from 'react';
-// const App = () => {
-//   const numbers = [7,8,9,4,5,6,1,2,3,0]
-
-//   return (
-//     <SafeAreaView style={styles.container}>
-//        {/* <TouchableOpacity>
-//         <Text style={styles.buttonText}>+</Text>
-//       </TouchableOpacity> */}
-
-//       {numbers.map((item, index) => {
-//         return <Reqemler reqem={item} />;
-//       })}
-// {/*
-//       // <TouchableOpacity>
-//       //   <Text style={styles.buttonText}>+</Text>
-//       // </TouchableOpacity> */}
-
-//     </SafeAreaView>
-//   );
-// };
-
-// export default App;
-// const Reqemler = props => {
-//   return(
-//   <TouchableOpacity style={styles.button}>
-//     <Text>{props.reqem}</Text>
-//   </TouchableOpacity>
-//   );
-//   };
-//   // export default App;
-//   const styles = StyleSheet.create ({
-//   container: {
-//     flex: 1,
-//     flexDirection: 'row',
-//     flexWrap: 'wrap',
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     alignContent: 'flex-end',
-//   },
-//   title: {
-//     fontSize: 30,
-//     marginBottom: 10,
-//   },
-//   button: {
-//     backgroundColor: 'orange',
-//     width: '30%',
-//     height: 85,
-//     borderRadius: 10,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//       marginBottom: 5,
-//       marginHorizontal: 3,
-
-//   },
-//   buttonText: {
-//     fontSize: 40,
-//     color: 'white',
-//   },
-// });
 
 import React from 'react';
 import {StyleSheet, SafeAreaView} from 'react-native';
-import HomePage from './HomePage';
-import SecondPage from './SecondPage';
-import Calculute from './Calculute';
+import Calculator  from './Calculator ';
+import Calculute  from './Calculute';
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
-      {/* <HomePage /> */}
-      {/* <SecondPage />  */}
-         <Calculute />
+      <Calculator />
+      {/* <Calculute /> */}
+      {/* <Kino /> */}
     </SafeAreaView>
   );
 };
--- src/Calculator .tsx
+++ src/Calculator .tsx
@@ -1,162 +1,166 @@
-import React from 'react';
-import {useState} from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 
-export default function Calculute() {
-  const [result, setResult] = useState('');
+export default function Calculator() {
+  const [calculationText, setCalculationText] = useState('');
+  const [resultText, setResultText] = useState('');
 
-  const onButtonClick = text => {
-    console.log(text);
-    if (text == '=') {
-      return calculation();
+  const onButtonPress = text => {
+    if (text === '=') {
+      return calculate();
     }
-    setResult(result + text);
+    setCalculationText(calculationText + text);
   };
 
-  const calculation = () => {
-    setResult(eval(result));
-  };
-  const onOperatorClick = text => {
-    if (text == 'C') {
-      return setResult(result.toString().substring(0, result.length - 1));
+  const onOperatorClick = operator => {
+    const lastChar = calculationText.slice(-1);
+
+    if (
+      lastChar === '+' ||
+      lastChar === '-' ||
+      lastChar === '*' ||
+      lastChar === '/'
+    ) {
+      setCalculationText(calculationText.slice(0, -1) + operator);
+      return;
     }
-    console.log(text);
-    setResult(result + text);
+
+    setCalculationText(calculationText + operator);
   };
+
+  const calculate = () => {
+    let uzunluq = calculationText;
+    let result = 0;
+    let operator = '+' || '*';
+    for (let i = 0; i < uzunluq.length; i++) {
+      const xarakter = uzunluq.charAt(i);
+      if (xarakter === '+' || xarakter === '-' || xarakter === '*' || xarakter === '/') {
+        operator = xarakter;
+      } else {
+        const number = parseFloat(uzunluq.slice(i));
+        if (!isNaN(number)) {
+          if (operator === '+') {
+            result += number;
+          } else if (operator === '-') {
+            result -= number;
+          } else if (operator === '*') {
+            result *= number;
+          } else {
+            result /= number;
+          }
+          i += number.toString().length - 1;
+        }
+      }
+    }
+    setResultText(result.toString());
+  };
+  
+  const onClearClick = () => {
+    setCalculationText('');
+    setResultText('');
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.calculation}>
-        <Text style={styles.calculationtext}>{result}</Text>
+        <Text style={styles.calculationtext}>{calculationText}</Text>
+        <Text style={styles.resulttext}>{resultText}</Text>
       </View>
       <View style={styles.buttons}>
         <View style={styles.numbers}>
           <View style={styles.row}>
             <TouchableOpacity
               style={styles.button}
-              onPress={() => {
-                onButtonClick(1);
-              }}>
-              <Text style={styles.number}>1</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={styles.button}
-              onPress={() => {
-                onButtonClick(2);
-              }}>
-              <Text style={styles.number}>2</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={styles.button}
-              onPress={() => {
-                onButtonClick(3);
-              }}>
-              <Text style={styles.number}>3</Text>
-            </TouchableOpacity>
-          </View>
-          <View style={styles.row}>
-            <TouchableOpacity
-              style={styles.button}
-              onPress={() => {
-                onButtonClick(4);
-              }}>
-              <Text style={styles.number}>4</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={styles.button}
-              onPress={() => {
-                onButtonClick(5);
-              }}>
-              <Text style={styles.number}>5</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={styles.button}
-              onPress={() => {
-                onButtonClick(6);
-              }}>
-              <Text style={styles.number}>6</Text>
-            </TouchableOpacity>
-          </View>
-          <View style={styles.row}>
-            <TouchableOpacity
-              style={styles.button}
-              onPress={() => {
-                onButtonClick(7);
-              }}>
+              onPress={() => onButtonPress('7')}>
               <Text style={styles.number}>7</Text>
             </TouchableOpacity>
             <TouchableOpacity
               style={styles.button}
-              onPress={() => {
-                onButtonClick(8);
-              }}>
+              onPress={() => onButtonPress('8')}>
               <Text style={styles.number}>8</Text>
             </TouchableOpacity>
             <TouchableOpacity
               style={styles.button}
-              onPress={() => {
-                onButtonClick(9);
-              }}>
+              onPress={() => onButtonPress('9')}>
               <Text style={styles.number}>9</Text>
             </TouchableOpacity>
           </View>
           <View style={styles.row}>
             <TouchableOpacity
               style={styles.button}
-              onPress={() => {
-                onButtonClick('.');
-              }}>
-              <Text style={styles.number}>.</Text>
+              onPress={() => onButtonPress('4')}>
+              <Text style={styles.number}>4</Text>
             </TouchableOpacity>
             <TouchableOpacity
               style={styles.button}
-              onPress={() => {
-                onButtonClick(0);
-              }}>
+              onPress={() => onButtonPress('5')}>
+              <Text style={styles.number}>5</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => onButtonPress('6')}>
+              <Text style={styles.number}>6</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.row}>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => onButtonPress('1')}>
+              <Text style={styles.number}>1</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => onButtonPress('2')}>
+              <Text style={styles.number}>2</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => onButtonPress('3')}>
+              <Text style={styles.number}>3</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.row}>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => onButtonPress('0')}>
               <Text style={styles.number}>0</Text>
             </TouchableOpacity>
             <TouchableOpacity
               style={styles.button}
-              onPress={() => {
-                onButtonClick('=');
-              }}>
-              <Text style={styles.number}>=</Text>
+              onPress={() => onButtonPress('.')}>
+              <Text style={styles.number}>.</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => onButtonPress('=')}>
+              <Text style={styles.operatorButtons}>=</Text>
             </TouchableOpacity>
           </View>
         </View>
         <View style={styles.operator}>
           <TouchableOpacity
             style={styles.button}
-            onPress={() => {
-              onOperatorClick('C');
-            }}>
+            onPress={() => onClearClick()}>
             <Text style={styles.operatorButtons}>C</Text>
           </TouchableOpacity>
           <TouchableOpacity
             style={styles.button}
-            onPress={() => {
-              onOperatorClick('/');
-            }}>
+            onPress={() => onOperatorClick('/')}>
             <Text style={styles.operatorButtons}>/</Text>
           </TouchableOpacity>
           <TouchableOpacity
             style={styles.button}
-            onPress={() => {
-              onOperatorClick('*');
-            }}>
-            <Text style={styles.operatorButtons}>*</Text>
+            onPress={() => onOperatorClick('*')}>
+            <Text style={styles.operatorButtons}>x</Text>
           </TouchableOpacity>
           <TouchableOpacity
             style={styles.button}
-            onPress={() => {
-              onOperatorClick('-');
-            }}>
+            onPress={() => onOperatorClick('-')}>
             <Text style={styles.operatorButtons}>-</Text>
           </TouchableOpacity>
           <TouchableOpacity
             style={styles.button}
-            onPress={() => {
-              onOperatorClick('+');
-            }}>
+            onPress={() => onOperatorClick('+')}>
             <Text style={styles.operatorButtons}>+</Text>
           </TouchableOpacity>
         </View>
@@ -191,6 +195,11 @@ const styles = StyleSheet.create({
   },
   calculationtext: {
     fontSize: 30,
+    fontWeight: 'bold',
+    color: 'black',
+  },
+  resulttext: {
+    fontSize: 24,
     fontWeight: 'bold',
     color: 'black',
   },
